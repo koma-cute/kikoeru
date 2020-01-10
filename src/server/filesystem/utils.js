7b3b56006f6edb2ade362783b40a47d3b85d5dd0
Xinghan <liuxinghan1998@gmail.com>
2020-01-10 18:56:09 +0800

    移除 throttlePromises 函数
    弃用 throttlePromises 函数，使用更方便的 limit-promise 模块进行"并发"控制。

--- src/server/filesystem/utils.js
+++ src/server/filesystem/utils.js
@@ -3,7 +3,7 @@ const path = require('path');
 const recursiveReaddir = require('recursive-readdir');
 const { orderBy } = require('natural-orderby');
 
-const config = require('../../../config.json');
+const config = require(process.cwd() + '/config.json');
 
 /**
  * Returns list of playable tracks in a given folder. Track is an object
@@ -49,19 +49,21 @@ const getTrackList = (id, dir) => recursiveReaddir(
 /**
  * Returns list of directory names (relative) that contain an RJ code.
  */
-async function* getFolderList(current = '', depth = 0) {
-  const folders = await fs.promises.readdir(path.join(config.rootDir, current));
+async function* getFolderList(current = '', depth = 0) { // 异步生成器函数 async function*() {}
+  // 浅层遍历rootDir路径
+  const folders = await fs.promises.readdir(path.join(config.rootDir, current));    
 
   for (const folder of folders) {
     const absolutePath = path.resolve(config.rootDir, current, folder);
     const relativePath = path.join(current, folder);
 
     // eslint-disable-next-line no-await-in-loop
-    if ((await fs.promises.stat(absolutePath)).isDirectory()) {
-      if (folder.match(/RJ\d{6}/)) {
+    if ((await fs.promises.stat(absolutePath)).isDirectory()) { // 检查是否为文件夹
+      if (folder.match(/RJ\d{6}/)) { // 检查文件夹名称中是否含有RJ号
         // Found a work folder, don't go any deeper.
         yield relativePath;
       } else if (depth + 1 < config.scannerMaxRecursionDepth) {
+        // 若文件夹名称中不含有RJ号，就进入该文件夹内部
         // Found a folder that's not a work folder, go inside if allowed.
         yield* getFolderList(relativePath, depth + 1);
       }
@@ -85,54 +87,27 @@ const deleteCoverImageFromDisk = rjcode => new Promise((resolve, reject) => {
 
 /**
  * Saves cover image to disk.
- * @param {ReadableStream} stream Image data stream.
+ * @param {ReadableStream} readerStream Image data stream.
  * @param {String} rjcode Work RJ code (only the 6 digits, zero-padded).
  */
-const saveCoverImageToDisk = (stream, rjcode) => new Promise((resolve, reject) => {
+const saveCoverImageToDisk = (readerStream, rjcode) => new Promise((resolve, reject) => {
   // TODO: don't assume image is a jpg?
+  var writerStream = fs.createWriteStream(path.join(config.rootDir, 'Images', `RJ${rjcode}.jpg`));
+  // 处理流事件 --> data, end, and error
+  writerStream.on('finish', () => resolve());
+
   try {
-    stream.pipe(
-      fs.createWriteStream(path.join(config.rootDir, 'Images', `RJ${rjcode}.jpg`))
-        .on('close', () => resolve()),
-    );
+    readerStream.pipe(writerStream);
   } catch (err) {
     reject(err);
   }
 });
 
-/**
- * Runs an array of Promise returning functions at a specified rate
- * @param {Number} the maximum number of unresolved promises that may exist at a given time
- * @param {Array<Function>} an array of promise-creating functions 
-*/
-const throttlePromises = (maxPending, asyncFuncs) => {
-  return new Promise((resolve, reject) => {
-      let numPending = 0;
-      let nextFuncId = 0;
-      const promisedValues = [];
-      (function check() {
-          if (nextFuncId >= asyncFuncs.length) { // All promises created
-              if (numPending == 0) resolve(promisedValues); // All promises fulfilled
-              return;
-          }
-          while (numPending < maxPending) { // Room for creating promise(s)
-              numPending++;
-              const thisFuncId = nextFuncId++;
-              asyncFuncs[thisFuncId]().then(value => {
-                  promisedValues[thisFuncId] = value;
-                  numPending--;
-                  check();
-              }).catch(reject);
-          }
-      })();
-  });
-};
-
-
+// 模块接口，声明这个模块对外暴露什么内容
 module.exports = {
   getTrackList,
   getFolderList,
   deleteCoverImageFromDisk,
   saveCoverImageToDisk,
-  throttlePromises
 };
+
