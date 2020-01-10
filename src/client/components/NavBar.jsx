41aa3fbb9dcd0c3ca4d9c50a4a88d4cb25be9b3e
Xinghan <liuxinghan1998@gmail.com>
2020-01-10 23:17:54 +0800

    添加 /scan 导航项

--- src/client/components/NavBar.jsx
+++ src/client/components/NavBar.jsx
@@ -6,9 +6,15 @@ const closeOffCanvasNav = () => {
   UIkit.offcanvas(element).hide();
 };
 
+/**
+ * 
+ * @param {*} props 
+ * 每个组件对象都会有props（properties的简写）属性,组件标签的所有属性都保存在props中
+ * 作用：通过标签属性从组件外 向组件内传递数据
+ */
 const OffCanvasNav = (props) => {
   const { location } = props;
-  const path = location.pathname;
+  const path = location.pathname; // 获取当前的路由地址
 
   return (
     <div id="offcanvas-nav" uk-offcanvas="mode: reveal; overlay: true; flip: true">
@@ -16,7 +22,9 @@ const OffCanvasNav = (props) => {
         <ul className="uk-nav uk-nav-primary uk-nav-center uk-margin-auto-vertical">
           <li className="k-logo k-logo-muted" />
           <li className="uk-nav-header">Browse</li>
+          
           <li className={path === '/' ? 'uk-active' : 'uk-parent'}>
+            {/**路径跳转组件Link */}
             <Link to="/" onClick={closeOffCanvasNav}>
               <span className="uk-margin-small-right" uk-icon="icon: thumbnails" />
               Works
@@ -43,11 +51,24 @@ const OffCanvasNav = (props) => {
               VAs
             </Link>
           </li>
+
+          <li className={path === '/scan/' ? 'uk-active' : 'uk-parent'}>
+            <Link to="/scan/" onClick={closeOffCanvasNav}>
+              <span className="uk-margin-small-right" uk-icon="icon: search" />
+              Scan
+            </Link>
+          </li>
         </ul>
       </div>
     </div>
   );
 };
+
+/**
+ * 将一个组件包裹进Route里面, 然后react-router的三个对象history, location, match就会被放进这个组件的props属性中.
+ * 所以withRouter的作用就是, 如果我们某个东西不是一个Router, 
+ * 但是我们要依靠它去跳转一个页面, 比如点击页面的logo, 返回首页, 这时候就可以使用withRouter来做.
+ */
 const OffCanvasNavWithRouter = withRouter(OffCanvasNav);
 
 const NavBar = (props) => {
